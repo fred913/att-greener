729b6071ac9be7dbbc2a2a46b2f73927527bc19c
Sheng Fan <fredtools999@gmail.com>
2025-05-13 08:54:51 +0800

    feat: export translation&pronunciation as lrc

--- src/injections/exportAs.ts
+++ src/injections/exportAs.ts
@@ -90,9 +90,150 @@ export const exportAsInjection: TypeInjection = {
             menu.itemsContainer.removeChild(item)
         }
 
-        for (const formatType of LyricFormat.allTypes) {
-            if (!LyricIO.Dumping.supportDump(formatType)) continue
+        const currAtt = getCurrentLyrics()
+        const currCl = currentLyricsToCoreLyric()
 
+        function handleExport(
+            mode: 'download' | 'clipboard',
+            formatType: LyricFormat.Type
+        ) {
+            closeCurrentRadixMenuView()
+            if (!currCl) {
+                Toastify({
+                    text: '当前无歌词数据，无法导出',
+                    duration: 2600,
+                    close: true,
+                    gravity: 'top',
+                    position: 'right',
+                    stopOnFocus: true,
+                    style: {
+                        background: 'rgb(18, 18, 18)',
+                        borderRadius: '16px',
+                        boxShadow: 'unset',
+                    },
+                }).showToast()
+                return
+            }
+
+            let footer = '',
+                header = ''
+            switch (formatType) {
+                case 'lyl':
+                    header += '[type:LyricifyLines]\n'
+                case 'lys':
+                case 'qrc':
+                case 'lrc':
+                    header += attMetaToLrcHeader(currAtt?.metadata || []).trim()
+                    header += '\n'
+                    break
+
+                case 'yrc':
+                    header += attMetaToYrcHeader(currAtt?.metadata || [])
+                        .map((d) => {
+                            return JSON.stringify(d)
+                        })
+                        .join('\n')
+                    header += '\n'
+                    break
+
+                case 'lqe':
+                    // [Lyricify Quick Export] // 作为统一的头部标记，用于识别
+                    // [version:1.0] // 此处记录 Lyricify Quick Export 的定义版本
+                    // [ti:xxx] // Tag 信息直接写在最前面，不放进歌词
+                    // [ar, al, by, ... 同理]
+                    header += '[Lyricify Quick Export]\n'
+                    header += '[version:1.0]\n'
+                    header += attMetaToLrcHeader(currAtt?.metadata || []).trim()
+                    header += '\n\n'
+                    break
+
+                default:
+                    break
+            }
+
+            let fixedCl
+            switch (formatType) {
+                case 'lys':
+                case 'qrc':
+                    // special fix
+                    // `\((\d+),(\d+)\) \(0,0\)` TO ` ($1,$2)`
+                    fixedCl = currCl.map(fixLineForLys)
+                    break
+                default:
+                    fixedCl = currCl
+                    break
+            }
+
+            const content =
+                header + LyricIO.Dumping.dump(formatType, fixedCl) + footer
+            const fileExtensions =
+                LyricFormat.getLyricFormatFileExtensions(formatType)
+            const dispName = LyricFormat.getLyricFormatDisplayName(formatType)
+
+            if (mode === 'clipboard') {
+                navigator.clipboard
+                    .writeText(content)
+                    .then(() => {
+                        Toastify({
+                            text: `歌词内容已复制为 ${dispName} 格式`,
+                            duration: 2600,
+                            close: true,
+                            gravity: 'top',
+                            position: 'right',
+                            stopOnFocus: true,
+                            style: {
+                                background: 'rgb(18, 18, 18)',
+                                borderRadius: '16px',
+                                boxShadow: 'unset',
+                            },
+                        }).showToast()
+                    })
+                    .catch(() => {
+                        Toastify({
+                            text: '复制失败',
+                            duration: 2000,
+                            close: true,
+                            gravity: 'top',
+                            position: 'right',
+                            stopOnFocus: true,
+                            style: {
+                                background: 'rgb(18, 18, 18)',
+                                borderRadius: '16px',
+                                boxShadow: 'unset',
+                            },
+                        }).showToast()
+                    })
+            } else {
+                LyricFormat.requestWriteLyricsFile(
+                    LyricFormat.getLyricFormatDisplayName(formatType),
+                    fileExtensions,
+                    content,
+                    { fileName: 'lyrics' }
+                ).then((success) => {
+                    Toastify({
+                        text: success
+                            ? `歌词已导出为 ${dispName} 格式`
+                            : '导出失败：已取消',
+                        duration: success ? 2600 : 1800,
+                        close: true,
+                        gravity: 'top',
+                        position: 'right',
+                        stopOnFocus: true,
+                        style: {
+                            background: 'rgb(18, 18, 18)',
+                            borderRadius: '16px',
+                            boxShadow: 'unset',
+                        },
+                    }).showToast()
+                })
+            }
+        }
+
+        function createExportAsMenuItem(
+            formatType: LyricFormat.Type,
+            lines?: CoreLyric.LineSyncedLine[],
+            overrideItemName?: string
+        ) {
             const dispName = LyricFormat.getLyricFormatDisplayName(formatType)
             const fileExt = LyricFormat.getLyricFormatFileExtensions(formatType)
 
@@ -106,7 +247,8 @@ export const exportAsInjection: TypeInjection = {
             item.setAttribute('role', 'menuitem')
             item.setAttribute('data-orientation', 'vertical')
             item.setAttribute('data-radix-collection-item', '')
-            item.innerText = `导出到 ${dispName} (${fileExt.join(', ')})`
+            item.innerText =
+                overrideItemName || `导出到 ${dispName} (${fileExt.join(', ')})`
             if (!LyricIO.Dumping.supportDump(formatType)) {
                 item.innerText += ' (尚未实现)'
             }
@@ -116,155 +258,52 @@ export const exportAsInjection: TypeInjection = {
             item.addEventListener('click', (ev) => {
                 // Skip if it's an auxiliary click (handled below)
                 if (ev instanceof MouseEvent && ev.button !== 0) return
-                handleExport('download')
+                handleExport('download', formatType)
             })
 
             item.addEventListener('contextmenu', (ev) => {
                 ev.preventDefault()
                 ev.stopPropagation()
-                handleExport('clipboard')
+                handleExport('clipboard', formatType)
                 return false
             })
 
-            function handleExport(mode: 'download' | 'clipboard') {
-                closeCurrentRadixMenuView()
-                const currAtt = getCurrentLyrics()
-                const currCl = currentLyricsToCoreLyric()
-                if (!currCl) {
-                    Toastify({
-                        text: '当前无歌词数据，无法导出',
-                        duration: 2600,
-                        close: true,
-                        gravity: 'top',
-                        position: 'right',
-                        stopOnFocus: true,
-                        style: {
-                            background: 'rgb(18, 18, 18)',
-                            borderRadius: '16px',
-                            boxShadow: 'unset',
-                        },
-                    }).showToast()
-                    return
-                }
+            return item
+        }
 
-                let footer = '',
-                    header = ''
-                switch (formatType) {
-                    case 'lyl':
-                        header += '[type:LyricifyLines]\n'
-                    case 'lys':
-                    case 'qrc':
-                    case 'lrc':
-                        header += attMetaToLrcHeader(
-                            currAtt?.metadata || []
-                        ).trim()
-                        header += '\n'
-                        break
+        for (const formatType of LyricFormat.allTypes) {
+            if (!LyricIO.Dumping.supportDump(formatType)) continue
 
-                    case 'yrc':
-                        header += attMetaToYrcHeader(currAtt?.metadata || [])
-                            .map((d) => {
-                                return JSON.stringify(d)
-                            })
-                            .join('\n')
-                        header += '\n'
-                        break
+            const item = createExportAsMenuItem(formatType)
+            menu.itemsContainer.appendChild(item)
+        }
 
-                    case 'lqe':
-                        // [Lyricify Quick Export] // 作为统一的头部标记，用于识别
-                        // [version:1.0] // 此处记录 Lyricify Quick Export 的定义版本
-                        // [ti:xxx] // Tag 信息直接写在最前面，不放进歌词
-                        // [ar, al, by, ... 同理]
-                        header += '[Lyricify Quick Export]\n'
-                        header += '[version:1.0]\n'
-                        header += attMetaToLrcHeader(
-                            currAtt?.metadata || []
-                        ).trim()
-                        header += '\n\n'
-                        break
-
-                    default:
-                        break
-                }
-
-                let fixedCl
-                switch (formatType) {
-                    case 'lys':
-                    case 'qrc':
-                        // special fix
-                        // `\((\d+),(\d+)\) \(0,0\)` TO ` ($1,$2)`
-                        fixedCl = currCl.map(fixLineForLys)
-                        break
-                    default:
-                        fixedCl = currCl
-                        break
-                }
-
-                const content =
-                    header + LyricIO.Dumping.dump(formatType, fixedCl) + footer
-                const fileExtensions =
-                    LyricFormat.getLyricFormatFileExtensions(formatType)
-
-                if (mode === 'clipboard') {
-                    navigator.clipboard
-                        .writeText(content)
-                        .then(() => {
-                            Toastify({
-                                text: `歌词内容已复制为 ${dispName} 格式`,
-                                duration: 2600,
-                                close: true,
-                                gravity: 'top',
-                                position: 'right',
-                                stopOnFocus: true,
-                                style: {
-                                    background: 'rgb(18, 18, 18)',
-                                    borderRadius: '16px',
-                                    boxShadow: 'unset',
-                                },
-                            }).showToast()
-                        })
-                        .catch(() => {
-                            Toastify({
-                                text: '复制失败',
-                                duration: 2000,
-                                close: true,
-                                gravity: 'top',
-                                position: 'right',
-                                stopOnFocus: true,
-                                style: {
-                                    background: 'rgb(18, 18, 18)',
-                                    borderRadius: '16px',
-                                    boxShadow: 'unset',
-                                },
-                            }).showToast()
-                        })
-                } else {
-                    LyricFormat.requestWriteLyricsFile(
-                        LyricFormat.getLyricFormatDisplayName(formatType),
-                        fileExtensions,
-                        content,
-                        { fileName: 'lyrics' }
-                    ).then((success) => {
-                        Toastify({
-                            text: success
-                                ? `歌词已导出为 ${dispName} 格式`
-                                : '导出失败：已取消',
-                            duration: success ? 2600 : 1800,
-                            close: true,
-                            gravity: 'top',
-                            position: 'right',
-                            stopOnFocus: true,
-                            style: {
-                                background: 'rgb(18, 18, 18)',
-                                borderRadius: '16px',
-                                boxShadow: 'unset',
-                            },
-                        }).showToast()
-                    })
-                }
+        if (currCl) {
+            const trans = CoreLyric.Utils.extractLineAnnotations(
+                currCl,
+                CoreLyric.LineAnnotationRole.Translation
+            )
+            if (trans.length > 0) {
+                const item = createExportAsMenuItem(
+                    'lrc',
+                    trans,
+                    '导出翻译为 LRC (.lrc)'
+                )
+                menu.itemsContainer.appendChild(item)
             }
 
-            menu.itemsContainer.appendChild(item)
+            const pron = CoreLyric.Utils.extractLineAnnotations(
+                currCl,
+                CoreLyric.LineAnnotationRole.Prononciation
+            )
+            if (pron.length > 0) {
+                const item = createExportAsMenuItem(
+                    'lrc',
+                    pron,
+                    '导出罗马音为 LRC (.lrc)'
+                )
+                menu.itemsContainer.appendChild(item)
+            }
         }
     },
 }
